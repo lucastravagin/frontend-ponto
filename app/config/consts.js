a8366d3647ceb99762d2047f0dae8095feab7ae7
oursys <admin@oursys.com.br>
2020-02-19 16:26:09 -0300

    first commit

--- app/config/consts.js
+++ app/config/consts.js
@@ -4,8 +4,8 @@
         version: '1.0',
         owner: 'Oursys',
         year: '2019',
-        apiUrl: 'http://localhost:4040/api',
-        oapiUrl: 'http://localhost:4040/oapi',
+        apiUrl: 'https://api-qrponto.oursys.com.br',
+        oapiUrl: 'https://api-qrponto.oursys.com.br/oapi',
         buscaCNPJ: 'https://www.receitaws.com.br/v1/cnpj',
         userKey: 'qr_ponto_user'
     }).run(['$rootScope', 'consts', function($rootScope, consts) {
